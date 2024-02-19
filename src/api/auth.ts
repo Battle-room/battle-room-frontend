cb4716760abb65c79b29be352b94cd9fb12c696d
Dmutre <dmytro.lesko@gmail.com>
2024-02-19 12:24:38 +0100

    Made correct request to server, fixed env variable

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -4,13 +4,13 @@ import { BASE_URL } from "./axios";
 const authPrefix = '/auth'
 
 export async function logIn(email: string, password: string) {
-  console.log(process.env);
   const url = BASE_URL + authPrefix + '/login';
-  console.log(url);
   const response = axios.post(url, {
       password,
-      email,
+      username: email,
     }
-  );
+  ).catch((error) => {
+    console.log(error);
+  });
   return response;
 }
