82ede18277d5edd14a80934b418a70591ef6700b
Dmutre <dmytro.lesko@gmail.com>
2024-02-19 11:06:21 +0100

    Added login form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,53 @@
 import Header from "../components/Header";
+import { useState } from "react";
+import "../styles/Login.css";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e: any) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: any) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(email + password);
+  };
+
   return (
-    <div className='login-page'>
+    <div>
       <Header />
+      <div className="login-container">
+        <div className="login-form-container">
+          <form onSubmit={handleSubmit} className="login-form">
+            <h2>Login</h2>
+            <div className="form-group">
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={handleEmailChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={handlePasswordChange}
+                required
+              />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
