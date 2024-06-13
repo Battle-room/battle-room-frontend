b247b10c7e32c08ec8eca915f5da932b45c7cf04
Dmutre <104783173+Dmutre@users.noreply.github.com>
2024-06-13 22:49:49 +0200

    completed authorization

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,52 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import HomePage from "./pages/HomePage";
-import './styles/App.css'
+import './styles/App.css';
 import LoginPage from "./pages/LoginPage";
-import SigninPage from "./pages/SigninPage";
-import { Provider } from "react-redux";
-import { userStore } from "./store/UserStore";
+import SignupPage from "./pages/SigninPage";
 import EmailVerifyPage from "./pages/EmailVerify";
+import { Provider, useDispatch } from "react-redux";
+import { userStore, UserStoreActions } from "./store/UserStore";
+import { getAccessToken } from "./services/CookieService";
+import { getMe } from "./api/auth";
+
+function AppRoutes() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkUser = async () => {
+      const token = getAccessToken();
+      if (token) {
+        try {
+          const response = await getMe(token);
+          dispatch({ type: UserStoreActions.SET_USER, payload: response.data });
+        } catch (error) {
+          console.error("Failed to authenticate user", error);
+          dispatch({ type: UserStoreActions.CLEAR_USER });
+        }
+      } else {
+        dispatch({ type: UserStoreActions.CLEAR_USER });
+      }
+    };
+
+    checkUser();
+  }, [dispatch, navigate]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/verify/email/:token" element={<EmailVerifyPage />} />
+    </Routes>
+  );
+}
 
 function App() {
   return (
     <Provider store={userStore}>
-      <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/login" element={<LoginPage />}/>
-        <Route path="/signin" element={<SigninPage />} />
-        <Route path="/verify/email/:token" element={<EmailVerifyPage />} />
-      </Routes>
+        <AppRoutes />
     </Provider>
   );
 }
